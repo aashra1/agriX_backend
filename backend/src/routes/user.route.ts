c7f11754319b51f1556bf75c3a3108da4d4a8ae9
aashra1 <aashrapandey00@gmail.com>
2026-02-17 14:23:22 +0545

    Add user order routes

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -6,23 +6,36 @@ import uploadProfilePicture from "../multer/user.multer";
 const router = Router();
 const userController = new UserController();
 
+router.get("/profile", authGuard, userController.getMyProfile);
+router.put(
+  "/profile",
+  authGuard,
+  uploadProfilePicture.single("profilePicture"),
+  userController.editMyProfile,
+);
+
 router.put("/change-password", authGuard, userController.changePassword);
-
 router.post("/request-password-reset", userController.sendResetPasswordEmail);
-
 router.post("/reset-password/:token", userController.resetPassword);
-
-router.get("/:userId", authGuard, userController.getUserProfile);
-
+router.get(
+  "/:userId",
+  authGuard,
+  authGuardAdmin,
+  userController.getUserProfile,
+);
 router.put(
   "/:userId",
   authGuard,
+  authGuardAdmin,
   uploadProfilePicture.single("profilePicture"),
   userController.editUserProfile,
 );
-
-router.delete("/:userId", authGuard, userController.deleteUserAccount);
-
+router.delete(
+  "/:userId",
+  authGuard,
+  authGuardAdmin,
+  userController.deleteUserAccount,
+);
 router.post(
   "/register",
   uploadProfilePicture.single("profilePicture"),
