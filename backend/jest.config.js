08efce80a67058201c55323bce80bdd7ea726b62
aashra1 <aashrapandey00@gmail.com>
2026-03-04 20:23:52 +0545

    Update Jest configuration

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,14 +1,29 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  roots: ["<rootDir>/src"],
-  testMatch: ["**/__tests__/**/*.test.ts"],
+  coverageProvider: "v8",
+  projects: [
+    {
+      displayName: "unit",
+      preset: "ts-jest",
+      testEnvironment: "node",
+      roots: ["<rootDir>/src"],
+      testMatch: ["**/__tests__/unit/**/*.test.ts"],
+      setupFilesAfterEnv: ["<rootDir>/src/__tests__/setup.unit.ts"],
+    },
+    {
+      displayName: "integration",
+      preset: "ts-jest",
+      testEnvironment: "node",
+      roots: ["<rootDir>/src"],
+      testMatch: ["**/__tests__/integration/**/*.test.ts"],
+      setupFilesAfterEnv: ["<rootDir>/src/__tests__/setup.ts"],
+      maxWorkers: 1,
+    },
+  ],
   collectCoverageFrom: [
     "src/**/*.ts",
     "!src/**/*.d.ts",
     "!src/index.ts",
-    "!src/server.ts",
+    "!src/app.ts",
     "!src/__tests__/**",
   ],
-  setupFilesAfterEnv: ["<rootDir>/src/__tests__/setup.ts"],
 };
